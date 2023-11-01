cffcda9b963f4ed959a8755fc3e7e900c79a8105
mhadrich <hadrichmalek@gmail.com>
2023-11-01 10:02:57 +0100

    commit config tailwind

--- mhportfolio/src/App.js
+++ mhportfolio/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import "./index.css"
 import NavBar from './Components/NavBar'
 
 function App() {
   return (
-    <div>
+    <div className="w-screen h-screen">
       <NavBar />
     </div>
   )
--- mhportfolio/src/Components/NavBar.js
+++ mhportfolio/src/Components/NavBar.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
 function NavBar() {
   return (
-    <div className=' w-96 bg-black h-24'>
-      <text>hello</text>
+    <div className="w-screen h-screen">
+      <div className="w- flex flex-col justify-between items-center bg-black h-24">
+        <text className="text-white">hello</text>
+        <text>hello</text>
+      </div>
     </div>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;

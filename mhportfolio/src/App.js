991b381701e18a933a413a7ab6d6190c76665bc9
mhadrich <hadrichmalek@gmail.com>
2023-11-01 13:30:53 +0100

    hero and stuff

--- mhportfolio/src/App.js
+++ mhportfolio/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import "./index.css"
 import NavBar from './Components/NavBar'
+import Hero from './Components/Hero'
+import ExternalLinks from './Components/ExternalLinks'
 
 function App() {
   return (
-    <div className="w-screen h-screen">
+    <div className='flex flex-col'>
       <NavBar />
+      <Hero />
+      <ExternalLinks />
     </div>
   )
 }
